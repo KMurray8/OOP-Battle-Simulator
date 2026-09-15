479f43b0b7dd8151eb823867286b4431f204a48b
KMurray8 <kayleemurray888@gmail.com>
2026-09-15 14:06:51 -0500

    Finished initial battle.

--- game.py
+++ game.py
@@ -4,6 +4,19 @@ from hero import Hero
 
 ARENA_NAME = "The Forest's Eye"
 
+def battle(hero: Hero, enemy: Goblin):
+    while hero.is_alive() and enemy.is_alive():
+        heroDamage = hero.attack()
+        enemy.take_damage(heroDamage)
+
+        if enemy.is_alive():
+            enemyDamage = enemy.attack()
+            hero.take_damage(enemyDamage)
+
+    if hero.is_alive():
+        print (f"{hero.name} wins!")
+    else:
+        print(f"{enemy.name} wins!")
 
 def main():
     """Open the arena and introduce its first opponent."""
@@ -13,15 +26,15 @@ def main():
     print("The gates are opening...")
 
     goblin = Goblin("Jeffy")
-
     goblin2 = Goblin("Scribble")
-
     hero = Hero("Lauriel")
+
     print(f"{hero.name} enters the arena with {hero.health} health.")
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print(f"{goblin2.name} enters the arena with {goblin2.health} health.")
+    print("      ")
+    battle(hero, goblin)
 
-    goblinHealth = goblin.health - hero.attack
 
 
 
