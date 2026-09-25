e8bf1bee48055e74f6f7145fd667e51e6fc6e056
KMurray8 <kayleemurray888@gmail.com>
2026-09-25 09:00:47 -0500

    Healing Potion now comment

--- hero.py
+++ hero.py
@@ -22,10 +22,10 @@ class Hero:
         """Returns True when the Hero has health remaining."""
         return self.health > 0
 
-    def healingPotion(self):
-        if self.health < 15:
-            if random.randint(1,2) == 2:
-                potionStrength = random.randint(1, 50)
-                self.health += potionStrength
-                print(f"{self.name} uses a healing potion, gaining {potionStrength}.")
-                print(f"{self.name}'s Health = {self.health}")
+    #def healingPotion(self):
+        #if self.health < 15:
+            #if random.randint(1,2) == 2:
+                #potionStrength = random.randint(1, 50)
+                #self.health += potionStrength
+                #print(f"{self.name} uses a healing potion, gaining {potionStrength}.")
+                #print(f"{self.name}'s Health = {self.health}")
